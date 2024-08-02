3dcf0c7f0b14df2e9044d282143fe97bbfc6ffbc
Yehor Tkachuk <tkachuk.egor2010@gmail.com>
2024-08-02 21:42:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+
+// 1
+
+// let group = [
+// {balance: 120 , friend : "Stockholm" , name : "Oslo" , skill : "Sense of humor"},
+// {balance: 130 , friend : "Oslo" , name : "Stockholm" , skill : "Strength"},
+// {balance: 110 , friend : "Oslo" , name : "London" , skill : "Smartness"},
+// {balance: 150 , friend : "Oslo" , name : "Paris" , skill : "Kindness"},
+// ]
+
+// let totalBalance = group.reduce((acc , item) => acc + item.balance , 0)
+
+// console.log(totalBalance)
+
+// 2
+
+// let group = [
+// {balance: 120 , friend : "Stockholm" , name : "Oslo" , skill : "Sense of humor"},
+// {balance: 130 , friend : "Oslo" , name : "Stockholm" , skill : "Strength"},
+// {balance: 110 , friend : "Oslo" , name : "London" , skill : "Smartness"},
+// {balance: 150 , friend : "Oslo" , name : "Paris" , skill : "Kindness"},
+// ]
+
+// let osloFriends = group 
+
+// .filter( x => x.friend === "Oslo") ;
+
+
+// console.log(osloFriends)
+
+// 3
+
+
+// let group = [
+// {balance: 120 , friend : "Stockholm" , friends : 12 , name : "Oslo" , skill : "Sense of humor"},
+// {balance: 130 , friend : "Oslo" , friends : 11 , name : "Stockholm" , skill : "Strength"},
+// {balance: 110 , friend : "Oslo" , friends : 10 , name : "London" , skill : "Smartness"},
+// {balance: 150 , friend : "Oslo" , friends : 1 , name : "Paris" , skill : "Kindness"},
+// ]
+
+// let sortingByFriends = (a,b) => a.friends - b.friends
+
+// console.log(group.sort(sortingByFriends))
+
+// 4
+
+
+
+
+
+
+
+
